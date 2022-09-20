bc3b8bf4da9e6ceaf07e7e2b6a04aec026e30327
Shota <1s2b1acksn0w@gmail.com>
2022-09-20 11:20:55 +0900

    テーブル作成、データの取得と表示

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -1,15 +1,26 @@
 package in.techcamp.firstapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+    private final PostRepository postRepository;
+
     @GetMapping("/hello")
-    public String showHello(Model model) {
+    public String showHello(Model model){
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);
         return "hello";
+    }
+
+    @GetMapping
+    public String showList(Model model){
+        var postList = postRepository.findAll();
+        model.addAttribute("postList", postList);
+        return "index";
     }
 }
